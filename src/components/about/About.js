a777bbb906d16c175170d16a6f23e0d8f7970a01
Akinwumi sehinde <102046091+Sahar009@users.noreply.github.com>
2024-01-17 10:08:04 +0100

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,16 +3,8 @@ import './about.css'
 
 const About = () => {
   return (
-    <div className='about container'>
-        <p>This is a final year project with Topic: <span>Design and implemention of block chain and authentication </span>
-        Designed by: <span> Emmanuel I Onyekwelu</span> with Matric number: <span> 1610 </span>from the Federal school of statictics. Ibadan</p>
-   <h1>Supervised by: <span>Mr Anselm Iheanyi Amadi </span></h1>
-   <h1>Year: <span>2023. </span></h1>
-   <h1>Stack for project</h1>
-   <p>HTML</p>
-   <p>CSS</p>
-   <p>React </p>
-   <p>Coin Gecko Api</p>
+  
+   <p>Just testing coin Gecko Api</p>
     </div>
   )
 }
